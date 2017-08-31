baf5689af24b6cb5f152b60434382573c0a50c57
aiyaya1993 <15958116062@163.com>
2017-08-31 19:52:48 +0800

    双向链表的实现

--- ceshi.js
+++ ceshi.js
@@ -14,8 +14,12 @@ function Node(ele){
 function Llist(){
 	this.head = new Node('head');
 	this.find = find;
+	this.findLast = findLast;
 	this.insert = insert;
 	this.remove = remove;
+	this.display = display;
+	this.dispReverse = dispReverse;
+	this.show = show;
 }
 //找到插入节点的位置
 function find(item){
@@ -39,8 +43,48 @@ function remove(item){
 	var a = currNode.previous;
 	a.next = currNode.next
 }
+//找到链表中最后一个节点
+function findLast() {
+	var currNode = new Node('head');
+	while(currNode.next != null){
+		currNode = currNode.next;
+	}
+	return currNode;
+}
+//正向读取链表元素
+function display() {
+	var currNode = this.head;
+	while(currNode.next != null){
+		console.log(currNode.ele);
+		currNode = currNode.next;
+	}
+	//console.log('display')
+}
+function dispReverse() {
+	var currNode = this.head;
+	currNode = this.findLast();
+	while(currNode.previous != null){
+		console.log(currNode.ele);
+		currNode = currNode.previous;
+	}
+}
+//当前节点在链表中向前移动n个节点
+function advance(n) {
+	//if
+}
+function show(item) {
+	var currNode = this.head;
+	while(currNode.next != null && currNode.ele != item ){
+		currNode = currNode.next;
+		//console.log
+	}
+	console.log(currNode.ele)
+	//return currNode;
+}
 var color = new Llist();
 color.insert('red','head');
 color.insert('blue','red');
 color.insert('yelllow','blue');
+//color.display();
 color.remove('red');
+color.show('yelllow');
