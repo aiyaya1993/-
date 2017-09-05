4f326544ea2b4298c2964c9d1c29c2cc22dc4048
aiyaya1993 <15958116062@163.com>
2017-09-05 22:41:47 +0800

    字典项的循环实现

--- dictionary.js
+++ dictionary.js
@@ -6,6 +6,7 @@ function Dictionary() {
 	this.datastore = new Array();
 	this.add = add;
 	this.showAll = showAll;
+	this.count = count;
 }
 //add方法
 function add(key , value) {
@@ -28,14 +29,50 @@ function showAll() {
 		console.log(this.datastore[i]);
 		console.log(678)
 	}*/
-	console.log(this.datastore)
+	/*console.log(this.datastore)
 	this.datastore.forEach(function(key) { //为什么foreach()方法不能用
 		console.log(datastore[key]);//
 		console.log(567)
-	})
+	})*/
+  /*Object.keys(this.datastore).forEach(function(key) {
+  	console.log(key + "->" + this.datastore[key]);
+  },this)*/
+  console.log(Object.keys(this.datastore).sort()) //['0':a,'1'"b"]
+	for(var key in this.datastore.sort()){
+		console.log(key + "->" + this.datastore[key]);
+		//console.log(n)	//0 1
+	}
 
+}
+//元素个数统计
+function count() {
+	var n = 0;
+	Object.keys(this.datastore).forEach(function(key) {
+		n++;
+	})
+	//console.log(n);
+}
+//删除所有元素
+function remove() {
+	Object.keys(this.datastore).forEach(function(key) {
+		delete this.datastore[key];
+	})
 }
 var book = new Dictionary();
 book.add('a',1);
 book.add('b',2);
 book.showAll();
+book.count();
+
+var obj = {
+	a:1,
+	b:'apple',
+	c:'hello',
+	d:2
+}
+for(var n in obj){
+	console.log(n)
+}
+/*for(var n in Object.keys(obj)){
+	console.log();
+}*/
