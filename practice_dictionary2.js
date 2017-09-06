566b83e7fe1f73e0bc94d7bcabda8ac0c5d66d3b
aiyaya1993 <15958116062@163.com>
2017-09-06 22:05:22 +0800

    课后练习2

---
+++ practice_dictionary2.js
@@ -0,0 +1,17 @@
+/*
+ *使用dictionary类写一段程序
+ *该程序用来存储一段文本中各
+ *个单词出现的次数，但是每个
+ *单词只显示一次
+*/
+// 首先分割文本中的单词
+function division(sentence) {
+	var arr = new Array(); //定义一个数组
+	strs = sentence.split(' ');
+	console.log(strs)
+	/*for(var i = 0; i<strs.length; i++){
+		arr[i]
+	}*/
+}
+var a = 'apple a hh ui';
+division(a);
