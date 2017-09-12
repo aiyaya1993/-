5f02d482036708c9e1f30d0ddf7b02075310c640
aiyaya1993 <15958116062@163.com>
2017-09-12 21:36:13 +0800

    整型数据散列化

---
+++ HashTable2.js
@@ -0,0 +1,24 @@
+/*散列化整型键*/
+/*
+ *使用的数据是学生的成绩
+ *随机产生一个九位数的键
+ *用以识别学生身份和一门成绩
+*/
+//生成随机数
+function getRandomInt(min, max) {
+	return Math.floor(Math.random()*(max - min + 1));
+}
+function getStuData(arr) {
+	for(var i = 0; i < arr.length; i++){
+		var num = "";
+		for(var j = 0; j <= 9; j++){  //内循环九位数的生成
+			num += Math.floor(Math.random()*10); //随机一位数
+			console.log(num)
+		}
+		num += getRandomInt(50,100);
+		arr[i] = num;
+	}
+	console.log(arr)
+}
+var a = [89,90,87,76,66,65,35];
+getStuData(a);
